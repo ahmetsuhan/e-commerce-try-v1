8c17e5982058e7f97497a51321239481cbdff585
Ahmet Suhan Oka <ahmetsuhanok0@gmail.com>
2021-03-21 00:19:37 +0300

    add directory and menu-item components

--- e-commerce-v1/src/pages/HomePage/index.js
+++ e-commerce-v1/src/pages/HomePage/index.js
@@ -1,47 +1,11 @@
 import React from 'react';
+import Directory from '../../components/Directory';
 import './homepage.scss';
 
 const HomePage = () => {
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">HATS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">JACKETS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">SNEAKERS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">WOMENS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">MENS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-
-                
-            </div>
+           <Directory/>
         </div>
     )
 }
