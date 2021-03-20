be6800a0e24eddb2cc2de482180a7c8fe1d06c72
Ahmet Suhan Oka <ahmetsuhanok0@gmail.com>
2021-03-20 21:58:41 +0300

    added files for homepage

--- e-commerce-v1/src/App.js
+++ e-commerce-v1/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/HomePage/index'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div >
+      <HomePage/>
     </div>
   );
 }
