d7b743383ca45afa5ec393103639b440c44e5731
Ahmet Suhan Oka <ahmetsuhanok0@gmail.com>
2021-03-21 03:33:14 +0300

    broken homepage into smaller component, finished styling menu-item component

--- e-commerce-v1/src/components/MenuItem/index.js
+++ e-commerce-v1/src/components/MenuItem/index.js
@@ -4,12 +4,12 @@ import './menu-item.scss';
 const MenuItem = ({title,imageUrl,size}) => {
     return (
         <div 
-            style={{
-                backgroundImage:`url(${imageUrl})`
-            }}
-        className={` ${size} menu-item`}>
+        className={` ${size} menu-item`} >
+            <div className='background-image' 
+            style={{backgroundImage:`url(${imageUrl})`}}
+            />
         <div className="content">
-            <h1 className="title">{title}</h1>
+            <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">SHOP NOW</span>
         </div>
     </div>
