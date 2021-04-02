2152d8a1aa5b12403ccf0fcf0530c703f26cf189
Ahmet Suhan Oka <ahmetsuhanok0@gmail.com>
2021-04-02 21:12:46 +0300

    update WithSpinner HOC

--- e-commerce-v1/src/components/WithSpinner/index.js
+++ e-commerce-v1/src/components/WithSpinner/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import {SpinnerContainer,SpinnerOverlay} from './withSpinner.styles';
-const WithSpinner =  (WrappedComponent) => ({isLoading,...otherProps}) => {
-    return isLoading ? (
-        <SpinnerOverlay>
-            <SpinnerContainer/>
-        </SpinnerOverlay>
-    ): (
-        <WrappedComponent {...otherProps}/>
-    )
+const WithSpinner =  (WrappedComponent) =>  {
+    const Spinner =({isLoading,...otherProps}) =>{
+        return isLoading ? (
+            <SpinnerOverlay>
+                <SpinnerContainer/>
+            </SpinnerOverlay>
+        ): (
+            <WrappedComponent {...otherProps}/>
+        )
+    }
+   return Spinner;
 }
 
 export default WithSpinner;
